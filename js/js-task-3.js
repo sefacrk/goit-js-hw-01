cc9a31b22755ea3b814735ec82326a46ec59aeec
sefacrk <sefacrk19@gmail.com>
2024-07-01 20:04:03 +0300

    task 3 updated

--- js/js-task-3.js
+++ js/js-task-3.js
@@ -2,7 +2,7 @@ function getElementWidth(content,padding,border) {
     const contentWidth = parseFloat(content);
     const paddingWidth = parseFloat(padding);
     const borderWidth = parseFloat(border);
-    const totalWidth = contentWidth + paddingWidth + borderWidth;
+    const totalWidth = contentWidth + paddingWidth + paddingWidth +borderWidth +borderWidth;
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
